2cb108a63624e11fab5ec19bdab0fbadfe833d7c
Brad Ganley <bradart.ink@gmail.com>
2021-01-22 08:40:21 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+const base = 'https://fortnite-api.com/v1/stats/br/v2?name=';
+
+class FortTable extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { updating: false,
+                  brad: {},
+                  ehren: {},
+                  cam: {} }
+  }
+  async getBrad(){
+    let res = await fetch(`${base}DampClamz`, { mode: 'cors',
+                                                  headers:{'Access-Control-Allow-Origin': 'https://fortniteberries.bradart.repl.co'} });
+    let json = await res.json();
+    await this.setState({ brad: json.data.stats.all.overall })
+    console.log(this.state.brad)
+  }
+  async getEhren(){
+    let res = await fetch(`${base}theehreng`, { mode: 'cors',
+                                                  headers:{'Access-Control-Allow-Origin': 'https://fortniteberries.bradart.repl.co'} });
+    let json = await res.json();
+    await this.setState({ ehren: json.data.stats.all.overall })
+    console.log(this.state.ehren)
+  }
+  async getCam(){
+    let res = await fetch(`${base}thecambulance14`, { mode: 'cors',
+                                                  headers:{'Access-Control-Allow-Origin': 'https://fortniteberries.bradart.repl.co'} });
+    let json = await res.json();
+    await this.setState({ cam: json.data.stats.all.overall })
+    console.log(this.state.cam)
+  }
+  async updateData(){
+    await this.getBrad();
+    await this.getEhren();
+    await this.getCam();
+  }
+  async componentDidMount(){
+    await this.getBrad();
+    await this.getEhren();
+    await this.getCam();
+  }
+
+  render() {
+    let { brad, ehren, cam } = this.state
+    return (      
+        <Container>
+        <Table variant="dark" striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Stat</th>
+              <th>Brad</th>
+              <th>Ehren</th>
+              <th>Cameron</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>Wins</th>
+              <td>{brad.wins}</td>
+              <td>{ehren.wins}</td>
+              <td>{cam.wins}</td>
+            </tr>
+            <tr>
+              <th>Kills</th>
+              <td>{brad.kills}</td>
+              <td>{ehren.kills}</td>
+              <td>{cam.kills}</td>
+            </tr>
+            <tr>
+              <th>Top 10's</th>
+              <td>{brad.top10}</td>
+              <td>{ehren.top10}</td>
+              <td>{cam.top10}</td>
+            </tr>
+            <tr>
+              <th>Win Rate</th>
+              <td>{brad.winRate}</td>
+              <td>{ehren.winRate}</td>
+              <td>{cam.winRate}</td>
+            </tr>
+            <tr>
+              <th>Matches Played</th>
+              <td>{brad.matches}</td>
+              <td>{ehren.matches}</td>
+              <td>{cam.matches}</td>
+            </tr>
+            <tr>
+              <th>Minutes Played</th>
+              <td>{brad.minutesPlayed}</td>
+              <td>{ehren.minutesPlayed}</td>
+              <td>{cam.minutesPlayed}</td>
+            </tr>
+          </tbody>
+        </Table>
+        
+        </Container>
+    )
+  }
+}
+class App extends React.Component {
+  render(){
+  return (
+    <div className="App">
+      <Button variant="light" onClick={ () => { this.updateData() } }> Update Data </Button>
+      <FortTable />
+    </div>
+  );
+  }
+}
+
+export default App;
